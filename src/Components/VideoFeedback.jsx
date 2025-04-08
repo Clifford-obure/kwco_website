712c08a87ae6a7d721f84168ff3e13239aabc920
clifford-obure <cliffobure@gmail.com>
2025-04-09 02:39:56 +0300

    latest

--- src/Components/VideoFeedback.jsx
+++ src/Components/VideoFeedback.jsx
@@ -34,19 +34,19 @@ function VideoFeedback() {
       src: VideoFeedback1,
       title: "Exceeding Expectations",
       description:
-        "Hear directly from students and alumni as they share their experiences, insights, and success stories with SPN Law Academy. Explore firsthand accounts of how our innovative approach has made a positive impact.",
+        "Hear directly from clients as they share their experiences, insights, and success stories with KWCO Law Firm. Explore firsthand accounts of how our innovative approach has made a positive impact.",
     },
     {
       src: VideoFeedback2,
-      title: "Transformative Learning",
+      title: "Transformative Legal Solutions",
       description:
-        "Discover how our courses and teaching methods have transformed the academic journeys of our students, leading to successful careers in law.",
+        "Discover how our legal services and expert team have transformed the journeys of our clients, leading to successful legal outcomes and debt resolution.",
     },
     {
       src: VideoFeedback3,
-      title: "Personal Growth",
+      title: "Personal and Professional Growth",
       description:
-        "Learn how SPN Law Academy has contributed to the personal and professional growth of our students, fostering a lifelong love for learning and excellence.",
+        "Learn how KWCO Law Firm has contributed to the personal and financial well-being of our clients, ensuring ethical and effective legal representation.",
     },
   ];
 
@@ -56,21 +56,20 @@ function VideoFeedback() {
         <div className="text-center pb-8">
           <h2 className="text-3xl font-bold text-[#2F4B79]">Video Feedback</h2>
           <h1 className="text-2xl font-semibold text-black">
-            Hear From Our Satisfied Customers
+            Hear From Our Satisfied Clients
           </h1>
           <h3 className="text-gray-700 mt-4 text-lg">
-            Hear directly from students and alumni as they share their
-            experiences, insights, and success stories with SPN Law Academy.
-            Explore firsthand accounts of how our innovative approach to legal
-            education has made a positive impact on their academic and
-            professional journeys.
+            Hear directly from clients as they share their experiences,
+            insights, and success stories with KWCO Law Firm. Explore firsthand
+            accounts of how our legal expertise has made a positive impact on
+            their legal and financial situations.
           </h3>
         </div>
         <div className="space-y-12 ">
           {videos.map((video, index) => (
             <div
               key={index}
-              className=" grid grid-cols md:grid-cols-2 flex-col   md:flex-row items-center bg-gray-50 rounded-lg shadow-md p-6"
+              className="grid grid-cols md:grid-cols-2 flex-col md:flex-row items-center bg-gray-50 rounded-lg shadow-md p-6"
             >
               <div className="flex-1 p-4 ">
                 <img
